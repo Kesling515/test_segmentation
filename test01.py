1de74ed269b2d58f41425502b0c02434a38da56e
Kesling515 <1500390370@qq.com>
2019-08-22 18:35:03 +0800

    预处理图片，将眼底血管图进行直方图均衡化

--- test01.py
+++ test01.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
 
+m1 = tf.constant([[3,3]])
+m2 = tf.constant([[3],[3]])
+product = tf.matmul(m1, m2)
+
+with tf.Session() as sess:
+    resualt = sess.run(product)
+    print(resualt)
